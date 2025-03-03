46561486bc2663d90ab30c0c522d87cc54c5f583
airdrophuntersid <airdrops.hunters.id@gmail.com>
2025-03-03 07:23:26 +0000

    up

---
+++ accounts.js
@@ -0,0 +1,7 @@
+export const accounts = [
+  { username: "EMAIL 1", password: "PASSWORD 1" },
+  { username: "EMAIL 2", password: "PASSWORD 2" },
+  { username: "EMAIL 3", password: "PASSWORD 3" },
+  { username: "EMAIL 4", password: "PASSWORD 4" },
+  { username: "EMAIL 5", password: "PASSWORD 5" },
+];
